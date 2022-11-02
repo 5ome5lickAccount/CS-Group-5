3536838a5e021f78b3a5639d4f9db9f29f981a4c
SamBrooksh <sambrooksh@gmail.com>
2022-11-02 14:29:43 -0600

    Generate information to fill out csv
    It currently saves it to a separate file, but the general employees csv is the one from the course, and the employee test is the one with the new information - it should be easy to change the genNewInfo file to either randomize the info again, or change a specific point

---
+++ genNewInfo.py
@@ -0,0 +1,40 @@
+import random as rand
+departments = ["Science", "Engineer", "Medical", "Command", "Security"]
+titles = ["Barbarian","Bard","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Wizard"]
+    
+with open("employees.csv", 'r') as f:
+    lines=f.readlines()
+newLine = []    
+for line in lines:
+    if line[0] == 'I':
+        newLine.append(line)
+        continue
+    data = line.split(',')
+    day = rand.randrange(1,30)
+    month = rand.randrange(1,12)
+    year = rand.randrange(1950, 2000)
+    dob =str(day)+"/"+str(month)+"/"+str(year)
+    ssn =str(rand.randrange(100, 999)) + "-" + str(rand.randrange(10,99)) + "-"+str(rand.randrange(1000,9999))
+    startdate = str(rand.randrange(1, 30)) + "/" + str(rand.randrange(1,12)) + "/"+str(rand.randrange(2000,2021))
+    has_dd=str(rand.randrange(0,2))
+    routing_num=str(rand.randrange(100000000,999999999))
+    acct_num=str(rand.randrange(100000000,999999999))
+    ismanager=str(rand.randrange(0,2))
+    isarchived=str(rand.randrange(0,2))
+    emptitle=rand.choice(titles)
+    department = rand.choice(departments)
+    officephone=str(rand.randrange(100,999)) + "-"+str(rand.randrange(100,999))+"-"+str(rand.randrange(1000,9999))
+    firstname = data[1].split(" ")[0]
+    lastname = data[1].split(" ")[1]
+    officeemail=lastname+firstname+"@uvu.edu"
+    newData= ""
+    for item in data:
+        newData += item+","
+    newData = newData[0:-2]
+    newData +=','+dob+','+ssn+','+startdate+','+has_dd+','+routing_num+','+acct_num+','+ismanager
+    newData += ','+isarchived+','+emptitle+','+department+','+officephone+','+officeemail+'\n'
+    newLine.append(newData)
+
+with open("employeestest.csv", 'w') as w:
+    for line in newLine:
+        w.write(line)
